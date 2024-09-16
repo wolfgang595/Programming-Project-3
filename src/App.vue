8311823d9d5dc67e8e2ca5113175b38b37c81219
Nathan Serna <nath2534@outlook.com>
2024-09-15 22:45:55 -0500

    Final commit, transfered project2 html to project3 VUE. also had to code in to include the external javascript file.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
-<script setup>
-import {ref} from 'vue'
-
-const name = ref('')
+<script>
+export default {
+  mounted() {
+    const plugin = document.createElement("script");
+    plugin.setAttribute(
+      "src",
+      "D:\School\Fall 2024\Advanced Web Design\CITP4316\programming-project-3\src\main.js"
+    );
+    plugin.async = true;
+    document.head.appendChild(plugin);
+  }
+};
 </script>
-
 <template>
- 
+ <h1>Stopwatch</h1>
+
+
+ <div id="timeContainer"> <!-- create box container for stopwatch  -->
+
+<div id="timeDisplay">00:00:00</div>" <!-- display stopwatch zeros -->
+
+<button id="startButton" class="timerButton">Start</button> <!--  create a start button -->
+<button id="pauseButton" class="timerButton">Pause</button> <!--  create a Pause button -->
+<button id="resetButton" class="timerButton">Reset</button> <!--  create a Reset button -->
+
+
+
+
+
+
+
+
+
+<!--   -->
+</div>
 </template>
 
 
